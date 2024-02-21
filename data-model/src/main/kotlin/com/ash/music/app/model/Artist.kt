5557a63a3d217379ee642aab818b0010b58a8e99
Ash French <ash.french1@gmail.com>
2024-02-21 13:12:15 +0000

    update model

--- data-model/src/main/kotlin/com/ash/music/app/model/Artist.kt
+++ data-model/src/main/kotlin/com/ash/music/app/model/Artist.kt
@@ -2,20 +2,9 @@ package com.ash.music.app.model
 
 import java.util.UUID
 
-sealed class Artist(
+data class Artist(
     open val artistID: ArtistId,
     open val artistName: String
 )
-
-data class SoloArtist(
-    override val artistID: ArtistId,
-    override val artistName: String
-) : Artist(artistID, artistName)
-
-data class GroupArtist(
-    override val artistID: ArtistId,
-    override val artistName: String,
-    val artists: List<Artist>
-) : Artist(artistID, artistName)
 
 typealias ArtistId = UUID
--- data-model/src/main/kotlin/com/ash/music/app/model/Playlist.kt
+++ data-model/src/main/kotlin/com/ash/music/app/model/Playlist.kt
@@ -3,7 +3,9 @@ package com.ash.music.app.model
 import java.util.UUID
 
 data class Playlist(
-    val playlistId: PlaylistId
+    val playlistId: PlaylistId,
+    val playlistName: String,
+    val tracks: TrackList
 )
 
 typealias PlaylistId = UUID
