ff1fb291089b14f5ff21eeb27c64ee464081ccea
Ash French <ash.french1@gmail.com>
2024-03-07 15:45:19 +0000

    update dependency resolution

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.2.2"
-    id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.spring") version "1.9.22"
+    alias(myplugins.plugins.kotlin)
 }
 
 group = "com.ash.music.app"
@@ -19,8 +16,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
-
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,22 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            library("swagger", "org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+        }
+
+        create("myplugins") {
+            plugin("springboot", "org.springframework.boot").version("3.2.2")
+            plugin("springbootdm", "io.spring.dependency-management").version("1.1.4")
+            plugin("kotlin", "org.jetbrains.kotlin.jvm").version("1.9.22")
+            plugin("kotlinspring", "org.jetbrains.kotlin.plugin.spring").version("1.9.22")
+        }
+    }
+}
+
 rootProject.name = "music-app"
 include("data-model")
 include("user-library")
