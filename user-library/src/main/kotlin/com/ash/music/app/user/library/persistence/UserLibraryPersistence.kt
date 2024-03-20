c6b54f3d5bed388378af9ac77aaecb51a2a9261d
Ash French <ash.french1@gmail.com>
2024-03-20 15:45:58 +0000

    initial add of H2 and R2DBC connections

--- user-library/src/main/kotlin/com/ash/music/app/user/library/persistence/UserLibraryPersistence.kt
+++ user-library/src/main/kotlin/com/ash/music/app/user/library/persistence/UserLibraryPersistence.kt
@@ -3,11 +3,19 @@ package com.ash.music.app.user.library.persistence
 import com.ash.music.app.model.TrackId
 import com.ash.music.app.model.UserId
 import org.springframework.stereotype.Component
+import org.springframework.r2dbc.core.DatabaseClient
 
 @Component
-class UserLibraryPersistence {
+class UserLibraryPersistence(
+    private val databaseClient: DatabaseClient
+) {
     fun likeTrack(userId: UserId, trackId: TrackId) {
-        TODO("Not yet implemented")
+        databaseClient.sql("INSERT INTO TABLE ....")
+            .bind("userId", userId)
+            .bind("trackId", trackId)
+            .then().doOnError {
+                throw it
+            }
     }
 
     fun removeLikeTrack(userId: UserId, trackId: TrackId) {
