7d66440bd7841bd9d2e3e252b8ef357ba3bc6391
Ash French <ash.french1@gmail.com>
2024-03-23 17:09:51 +0000

    add in user artist persistent layer

--- user-library/src/main/kotlin/com/ash/music/app/user/library/endpoints/UserLibraryEndpoints.kt
+++ user-library/src/main/kotlin/com/ash/music/app/user/library/endpoints/UserLibraryEndpoints.kt
@@ -2,6 +2,7 @@ package com.ash.music.app.user.library.endpoints
 
 import com.ash.music.app.model.*
 import com.ash.music.app.user.library.persistence.UserAlbumPersistence
+import com.ash.music.app.user.library.persistence.UserArtistPersistence
 import com.ash.music.app.user.library.persistence.UserTrackPersistence
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
@@ -13,19 +14,20 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("user/{userId}")
 class UserLibraryEndpoints(
     private val userAlbumPersistence: UserAlbumPersistence,
-    private val userTrackPersistence: UserTrackPersistence
+    private val userTrackPersistence: UserTrackPersistence,
+    private val userArtistPersistence: UserArtistPersistence
 ) {
 
     private val logger = LoggerFactory.getLogger(UserLibraryEndpoints::class.java)
 
     @GetMapping
     fun getArtists(@PathVariable userId: UserId): IArtist {
-        TODO()
+        return userArtistPersistence.getArtists(userId)
     }
 
     @GetMapping("/artist/{artistId}")
     fun getArtist(@PathVariable userId: UserId, @PathVariable artistId: ArtistId): IArtist {
-        TODO()
+        return userArtistPersistence.getArtist(userId, artistId)
     }
 
     @GetMapping("/track")
--- user-library/src/main/kotlin/com/ash/music/app/user/library/persistence/UserArtistPersistence.kt
+++ user-library/src/main/kotlin/com/ash/music/app/user/library/persistence/UserArtistPersistence.kt
@@ -1,6 +1,7 @@
 package com.ash.music.app.user.library.persistence
 
 import com.ash.music.app.model.ArtistId
+import com.ash.music.app.model.IArtist
 import com.ash.music.app.model.UserId
 import org.springframework.stereotype.Component
 
@@ -19,6 +20,14 @@ class UserArtistPersistence {
     }
 
     fun removeDislikeArtist(userId: UserId, artistId: ArtistId) {
+        TODO("Not yet implemented")
+    }
+
+    fun getArtists(userId: UserId): IArtist {
+        TODO("Not yet implemented")
+    }
+
+    fun getArtist(userId: UserId, artistId: ArtistId): IArtist {
         TODO("Not yet implemented")
     }
 
