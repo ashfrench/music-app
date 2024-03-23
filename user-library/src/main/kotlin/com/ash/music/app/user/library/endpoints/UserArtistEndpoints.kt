f5ee12cbeb14efec286b5889e6a14d63f6905ec0
Ash French <ash.french1@gmail.com>
2024-03-23 17:08:07 +0000

    add in user artist persistent layer

--- user-library/src/main/kotlin/com/ash/music/app/user/library/endpoints/UserArtistEndpoints.kt
+++ user-library/src/main/kotlin/com/ash/music/app/user/library/endpoints/UserArtistEndpoints.kt
@@ -2,32 +2,39 @@ package com.ash.music.app.user.library.endpoints
 
 import com.ash.music.app.model.ArtistId
 import com.ash.music.app.model.UserId
+import com.ash.music.app.user.library.persistence.UserArtistPersistence
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("user/{userId}/artist/{artistId}")
-class UserArtistEndpoints {
+class UserArtistEndpoints(
+    private val userArtistPersistence: UserArtistPersistence
+) {
 
     private val logger = LoggerFactory.getLogger(UserArtistEndpoints::class.java)
 
     @PutMapping("/like")
     fun likeArtist(@PathVariable userId: UserId, @PathVariable artistId: ArtistId) {
-        TODO()
+        logger.info("User: [$userId] Liked Artist [$artistId]")
+        userArtistPersistence.likeArtist(userId, artistId)
     }
 
     @DeleteMapping("/like")
     fun removeLikeArtist(@PathVariable userId: UserId,@PathVariable  artistId: ArtistId) {
-        TODO()
+        logger.info("User: [$userId] Removed Liked Artist [$artistId]")
+        userArtistPersistence.removeLikeArtist(userId, artistId)
     }
 
     @PutMapping("/dislike")
     fun dislikeArtist(@PathVariable userId: UserId,@PathVariable  artistId: ArtistId) {
-        TODO()
+        logger.info("User: [$userId] Disliked Artist [$artistId]")
+        userArtistPersistence.dislikeArtist(userId, artistId)
     }
 
     @DeleteMapping("/dislike")
     fun removeDislikeArtist(@PathVariable userId: UserId,@PathVariable  artistId: ArtistId) {
-        TODO()
+        logger.info("User: [$userId] Removed Disliked Artist [$artistId]")
+        userArtistPersistence.removeDislikeArtist(userId, artistId)
     }
 }
---
+++ user-library/src/main/kotlin/com/ash/music/app/user/library/persistence/UserArtistPersistence.kt
@@ -0,0 +1,25 @@
+package com.ash.music.app.user.library.persistence
+
+import com.ash.music.app.model.ArtistId
+import com.ash.music.app.model.UserId
+import org.springframework.stereotype.Component
+
+@Component
+class UserArtistPersistence {
+    fun likeArtist(userId: UserId, artistId: ArtistId) {
+        TODO("Not yet implemented")
+    }
+
+    fun removeLikeArtist(userId: UserId, artistId: ArtistId) {
+        TODO("Not yet implemented")
+    }
+
+    fun dislikeArtist(userId: UserId, artistId: ArtistId) {
+        TODO("Not yet implemented")
+    }
+
+    fun removeDislikeArtist(userId: UserId, artistId: ArtistId) {
+        TODO("Not yet implemented")
+    }
+
+}
