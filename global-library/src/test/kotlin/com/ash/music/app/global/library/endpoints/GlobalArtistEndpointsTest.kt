e86a4bfa3bda6a4c8616a741b0ea13164d74d68e
Ash French <ash.french1@gmail.com>
2024-05-11 18:11:41 +0100

    tidy up test

--- global-library/src/test/kotlin/com/ash/music/app/global/library/endpoints/GlobalArtistEndpointsTest.kt
+++ global-library/src/test/kotlin/com/ash/music/app/global/library/endpoints/GlobalArtistEndpointsTest.kt
@@ -1,16 +1,14 @@
 package com.ash.music.app.global.library.endpoints
 
-import com.ash.music.app.model.IArtist
-import com.ash.music.app.model.SoloArtist
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.http.HttpStatus
-import java.util.UUID
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GlobalArtistEndpointsTest(@Autowired val restTemplate: TestRestTemplate) {
@@ -51,9 +49,6 @@ class GlobalArtistEndpointsTest(@Autowired val restTemplate: TestRestTemplate) {
 
     @Test
     fun `get artist tracks`() {
-
-        val x = UUID.fromString("11111111-1111-1111-1111-11111111111")
-
         val responseEntity = restTemplate.getForEntity<String>("/library/artist/deadbeef-dead-beef-dead-beefdeadbeef/track")
 
         assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
