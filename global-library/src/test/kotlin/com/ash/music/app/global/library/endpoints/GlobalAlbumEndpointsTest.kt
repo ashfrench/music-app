a6fc760d0ebba6f6203a8043418301ac2ddd6fbf
Ash French <ashfrench@users.noreply.github.com>
2024-05-15 07:23:40 +0100

    more tests

--- global-library/src/test/kotlin/com/ash/music/app/global/library/endpoints/GlobalAlbumEndpointsTest.kt
+++ global-library/src/test/kotlin/com/ash/music/app/global/library/endpoints/GlobalAlbumEndpointsTest.kt
@@ -43,4 +43,10 @@ class GlobalAlbumEndpointsTest(@Autowired val restTemplate: TestRestTemplate) {
         assertEquals("""[{"trackId":"11111111-1111-1111-1111-011111111111","trackName":"My Song","trackNumber":1,"albumId":"deadbeef-dead-beef-dead-beefdeadbeef","artistId":"deadcafe-dead-cafe-dead-cafedeadcafe"},{"trackId":"22222222-2222-2222-2222-022222222222","trackName":"I did a music","trackNumber":2,"albumId":"deadbeef-dead-beef-dead-beefdeadbeef","artistId":"deadcafe-dead-cafe-dead-cafedeadcafe"},{"trackId":"33333333-3333-3333-3333-033333333333","trackName":"Notes Happened","trackNumber":3,"albumId":"deadbeef-dead-beef-dead-beefdeadbeef","artistId":"deadcafe-dead-cafe-dead-cafedeadcafe"},{"trackId":"44444444-4444-4444-4444-044444444444","trackName":"Boom Another Song","trackNumber":4,"albumId":"deadbeef-dead-beef-dead-beefdeadbeef","artistId":"deadcafe-dead-cafe-dead-cafedeadcafe"},{"trackId":"55555555-5555-5555-5555-055555555555","trackName":"Loud Noises","trackNumber":5,"albumId":"deadbeef-dead-beef-dead-beefdeadbeef","artistId":"deadcafe-dead-cafe-dead-cafedeadcafe"}]""", responseEntity.body)
     }
 
+    @Test
+    fun `get album not found`() {
+        val responseEntity = restTemplate.getForEntity<String>("/library/album/deadcafe-dead-cafe-dead-cafedeadaaaa")
+        assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
+    }
+
 }
